2ddbdab36ba26dd24929ac11484a04111687b391
lizejun <846562928@qq.coom>
2019-10-05 11:56:53 +0800

    增加数组模拟栈的遍历、入栈和出栈

---
+++ src/main/java/com/lzj/stack/ArrayStack.java
@@ -0,0 +1,68 @@
+package com.lzj.stack;
+
+/**
+ * 使用数组模拟栈。
+ *
+ * @Author Sakura
+ * @Date 2019/10/5 11:28
+ */
+public class ArrayStack {
+    private int maxSize; // 栈的最大容量。
+    private int top = -1; // 栈顶指针，初始为 -1。
+    private int[] stack;
+
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new int[maxSize];
+    }
+
+    /**
+     * 入栈。
+     * <p>
+     * 从栈顶入。
+     *
+     * @param e 待添加元素。
+     */
+    public void push(int e) {
+        if (isFull()) {
+            System.out.println("栈满，无法入栈");
+            return;
+        }
+        top++;
+        stack[top] = e;
+    }
+
+    /**
+     * 出栈。
+     * <p>
+     * 从栈顶出。
+     *
+     * @return 栈顶元素。
+     */
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("栈空，无法出栈");
+        }
+        int value = stack[top];
+        top--;
+        return value;
+    }
+
+    public void list() {
+        if (isEmpty()) {
+            System.out.println("栈空，没有元素");
+            return;
+        }
+        for (int i = top; i >= 0; i--) {
+            System.out.printf("stack[%d]的值为 %d \n", i, stack[i]);
+        }
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+}
---
+++ src/main/java/com/lzj/stack/ArrayStackTest.java
@@ -0,0 +1,52 @@
+package com.lzj.stack;
+
+import java.util.Scanner;
+
+/**
+ * 数组模拟栈的测试用例。
+ *
+ * @Author Sakura
+ * @Date 2019/10/5 11:38
+ */
+public class ArrayStackTest {
+    public static void main(String[] args) {
+        ArrayStack arrayStack = new ArrayStack(4);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("list 遍历栈");
+            System.out.println("push 入栈");
+            System.out.println("pop 出栈");
+            System.out.println("exit 退出程序");
+
+            String key = scanner.nextLine();
+            switch (key) {
+                case "list":
+                    arrayStack.list();
+                    break;
+                case "push":
+                    arrayStack.push(scanner.nextInt());
+                    break;
+                case "pop":
+                    try {
+                        int value = arrayStack.pop();
+                        System.out.printf("出栈元素为 %d \n", value);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "exit":
+                    loop = false;
+                    scanner.close();
+                    System.out.println("程序已退出");
+                    break;
+                //default:
+                //    loop = false;
+                //    scanner.close();
+                //    System.out.println("输入有误，程序退出");
+                //    break;
+            }
+        }
+    }
+}
