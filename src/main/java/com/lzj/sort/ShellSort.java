53fd3c8820de54e64d8275ad16829304071ace29
lizejun <846562928@qq.com>
2019-10-14 15:22:09 +0800

    Update ShellSort.java
    增加交换式希尔排序的实现。

--- src/main/java/com/lzj/sort/ShellSort.java
+++ src/main/java/com/lzj/sort/ShellSort.java
@@ -6,26 +6,36 @@ import java.util.Arrays;
  * 希尔排序。
  * <p>
  * 希尔排序是插入排序的增强版。
+ * 
+ * 插入排序有两种方式，交换式和位移式。交换式相较于位移式，耗费时间较多。
+ * 同理，希尔排序也有两种方式。此处属于交换式希尔排序。
  *
  * @Author Sakura
  * @Date 2019/10/12 7:43
  */
 public class ShellSort {
     public static void main(String[] args) {
-        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000);
+        }
+        long start = System.currentTimeMillis();
         shellSort(arr);
-        System.out.println(Arrays.toString(arr));
+        long end = System.currentTimeMillis();
+        System.out.printf("消耗时间：%f ", (end - start) / 1000.0);
     }
 
     private static void shellSort(int[] arr) {
-        int temp;
-        // 十个数，分成五组，每组进行比较。
-        for (int i = 0; i < 5; i++) {
-            for (int j = i; j < arr.length; j += 4) {
-                if (arr[j] > arr[j + 4]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 4];
-                    arr[j + 4] = temp;
+        int temp = 0;
+        for (int gap = arr.length / 2; gap > 0 ; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0 ; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
                 }
             }
         }
