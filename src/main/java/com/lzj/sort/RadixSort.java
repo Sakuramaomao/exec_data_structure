bf1ea61aa34d2dd1b030f8cb3b34c4d5d1a7c4a1
lizejun <846562928@qq.com>
2019-10-18 11:41:09 +0800

    增加基数排序的实现和性能测试

--- src/main/java/com/lzj/sort/RadixSort.java
+++ src/main/java/com/lzj/sort/RadixSort.java
@@ -14,33 +14,77 @@ import java.util.Arrays;
  */
 public class RadixSort {
     public static void main(String[] args) {
-        int[] arr = {53, 3, 542, 748, 14, 214};
+//        int[] arr = {53, 3, 542, 748, 14, 214};
+        int[] arr = new int[80000000];
+        // 80000000 * 11 * 4 / 1024 / 1024 / 1024 = 3.27GB
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 8000000);
+        }
+        long start = System.currentTimeMillis();
         radixSort(arr);
-        System.out.println(Arrays.toString(arr));
+        long end = System.currentTimeMillis();
+        System.out.printf("消耗时间：%f", (end -start) / 1000.0);
+//        System.out.println(Arrays.toString(arr));
     }
 
     private static void radixSort(int[] arr) {
-        // 创建10个桶，只能确定行数，列数无法确定，只能先定这么多。
-        int[][] bucket = new int[10][20];
+        // 创建10个桶，只能确定行数，列数固定为数组长度。
+        int[][] bucket = new int[10][arr.length];
         // 用来记录每个桶中元素的个数。
         int[] bucketElemCount = new int[10];
 
-        // 第一趟
-        // 入桶
-        for (int i = 0; i < arr.length - 1; i++) {
-            // 遍历个位
-            int item = arr[i] % 10;
-            bucket[item][bucketElemCount[item]] = arr[i]; // 放入第item个桶中。
-            bucketElemCount[item]++; // 记录每个桶中的元素个数。
-        }
-
-        // 出桶
-        for (int i = 0; i < bucketElemCount.length - 1; i++) {
-            if (bucketElemCount[i] != 0) {
-                for (int j = 0; j < bucketElemCount[i]; j++) {
-                    arr[j] = bucket[i][j];
-                }
+        // 1、获取数组中最大值元素
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
+//        System.out.println(max);
+
+        // 2、获取最大值的位数，用来决定排序几趟。
+        int sortTimes = String.valueOf(max).length();
+
+        // 3、排序
+        for (int k = 0, n = 1; k < sortTimes; k++, n *= 10) {
+            // 入桶
+            for (int i = 0; i < arr.length; i++) {
+                // 遍历个位
+                int item = arr[i] / n % 10;
+                bucket[item][bucketElemCount[item]] = arr[i]; // 放入第item个桶中。
+                bucketElemCount[item]++; // 记录每个桶中的元素个数。
+            }
+
+            // 出桶
+            int t = 0;
+            for (int i = 0; i < bucketElemCount.length - 1; i++) { // 第i个桶
+                if (bucketElemCount[i] != 0) {
+                    for (int j = 0; j < bucketElemCount[i]; j++) { // 取出桶中元素放回原来的数组。
+                        arr[t++] = bucket[i][j];
+                    }
+                }
+                bucketElemCount[i] = 0; // 将每个桶中的元素数量记录清空，用来给下一趟排序使用。
+            }
+        }
+
+//        // 第一趟
+//        // 入桶
+//        for (int i = 0; i < arr.length; i++) {
+//            // 遍历个位
+//            int item = arr[i] / 1 % 10;
+//            bucket[item][bucketElemCount[item]] = arr[i]; // 放入第item个桶中。
+//            bucketElemCount[item]++; // 记录每个桶中的元素个数。
+//        }
+//
+//        // 出桶
+//        int k = 0;
+//        for (int i = 0; i < bucketElemCount.length - 1; i++) { // 第i个桶
+//            if (bucketElemCount[i] != 0) {
+//                for (int j = 0; j < bucketElemCount[i]; j++) { // 取出桶中元素放回原来的数组。
+//                    arr[k++] = bucket[i][j];
+//                }
+//            }
+//            bucketElemCount[i] = 0; // 将每个桶中的元素数量记录清空，用来给下一趟排序使用。
+//        }
     }
 }
