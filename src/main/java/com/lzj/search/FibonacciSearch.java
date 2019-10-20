0df2fbca8a5020614344b1067f584d0415c62eb0
lizejun <846562928@qq.coom>
2019-10-20 17:58:32 +0800

    增加斐波那锲数列生成的方法。

--- src/main/java/com/lzj/search/FibonacciSearch.java
+++ src/main/java/com/lzj/search/FibonacciSearch.java
@@ -1,5 +1,7 @@
 package com.lzj.search;
 
+import java.util.Arrays;
+
 /**
  * 斐波那锲（黄金分割）查找算法。待实现
  *
@@ -7,7 +9,24 @@ package com.lzj.search;
  * @Date 2019/10/20 16:31
  */
 public class FibonacciSearch {
-    public static void main(String[] args) {
+    private static int maxSize = 20;
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(fib()));
+    }
+
+    /**
+     * 生成斐波那锲数列
+     *
+     * @return 斐波那锲数列。
+     */
+    public static int[] fib() {
+        int[] f = new int[maxSize];
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i < maxSize; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        return f;
     }
 }
