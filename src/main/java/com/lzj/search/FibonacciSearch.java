6c61fdf1e90e948975c73388ae5ffd22104586f3
lizejun <846562928@qq.coom>
2019-10-20 17:50:33 +0800

    添加待实现的斐波那锲查找

---
+++ src/main/java/com/lzj/search/FibonacciSearch.java
@@ -0,0 +1,13 @@
+package com.lzj.search;
+
+/**
+ * 斐波那锲（黄金分割）查找算法。待实现
+ *
+ * @Author Sakura
+ * @Date 2019/10/20 16:31
+ */
+public class FibonacciSearch {
+    public static void main(String[] args) {
+
+    }
+}
