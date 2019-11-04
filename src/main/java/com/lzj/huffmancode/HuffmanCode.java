0f02cc128e00e3711dc350239e644bf7ba4b14f3
lizejun <846562928@qq.coom>
2019-11-04 22:08:39 +0800

    增加赫夫曼编码表的实现。

--- src/main/java/com/lzj/huffmancode/HuffmanCode.java
+++ src/main/java/com/lzj/huffmancode/HuffmanCode.java
@@ -1,9 +1,6 @@
 package com.lzj.huffmancode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 数据压缩的实现--赫夫曼编码实战
@@ -18,6 +15,10 @@ public class HuffmanCode {
         byte[] bytes = str.getBytes();
         Node huffmanTreeRoot = createHuffmanTree(getNodes(bytes));
         huffmanTreeRoot.preOrder();
+
+        // 2、构建赫夫曼编码表。
+        Map<Byte, String> huffmanCode = getCodes(huffmanTreeRoot);
+        System.out.println(huffmanCode);
     }
 
     // 创建huffman树。
@@ -28,7 +29,7 @@ public class HuffmanCode {
             // 2、取出第一个和第二个。构建二叉树。
             Node leftNode = nodes.get(0);
             Node rightNode = nodes.get(1);
-            Node parentNode = new Node((byte) 1, leftNode.weight + rightNode.weight);
+            Node parentNode = new Node(null, leftNode.weight + rightNode.weight);
             parentNode.left = leftNode;
             parentNode.right = rightNode;
             // 3、删除第一个和第二个，添加新的。
@@ -37,6 +38,44 @@ public class HuffmanCode {
             nodes.add(parentNode);
         }
         return nodes.get(0);
+    }
+
+    // 重载以下getCodes方法，方便使用。
+    public static Map<Byte, String> getCodes(Node root) {
+        if (root == null) {
+            return huffmanCodes;
+        } else {
+            getCodes(root.left, "0", codeBuilder);
+            getCodes(root.right, "1", codeBuilder);
+        }
+        return huffmanCodes;
+    }
+
+    // 生成huffman编码表。
+    // 1、将生成的huffman编码表存放到 Map<Byte, String> 形式的集合中。
+    private static final Map<Byte, String> huffmanCodes = new HashMap();
+    // 2、在生成huffman编码时，需要拼接路径，定义一个StringBuilder，存储某个叶子节点的路径。
+    private static final StringBuilder codeBuilder = new StringBuilder();
+
+    /**
+     * 递归的方式生成huffman编码表。
+     *
+     * @param node          传入节点。
+     * @param code          左子节点线：0；右子节点线：1。
+     * @param stringBuilder huffman编码的路径拼接。
+     */
+    private static void getCodes(Node node, String code, StringBuilder stringBuilder) {
+        StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+        stringBuilder2.append(code);
+
+        if (node != null) { // 如果node == null，不处理。
+            if (node.data == null) {
+                getCodes(node.left, "0", stringBuilder2);
+                getCodes(node.right, "1", stringBuilder2);
+            } else {
+                huffmanCodes.put(node.data, stringBuilder2.toString());
+            }
+        }
     }
 
     /**
@@ -73,13 +112,13 @@ public class HuffmanCode {
 
 // 描述huffman树中节点的node类。
 class Node implements Comparable<Node> {
-    byte ch; // 字符对应的ASCII码。
+    Byte data; // 字符对应的ASCII码。
     int weight; // 权重，记录字符出现的次数。
     Node left; // 左子节点。
     Node right; // 右子节点。
 
-    public Node(byte ch, Integer weight) {
-        this.ch = ch;
+    public Node(Byte data, Integer weight) {
+        this.data = data;
         this.weight = weight;
     }
 
@@ -97,7 +136,7 @@ class Node implements Comparable<Node> {
     @Override
     public String toString() {
         return "Node{" +
-                "ch=" + ch +
+                "data=" + data +
                 ", weight=" + weight +
                 '}';
     }
