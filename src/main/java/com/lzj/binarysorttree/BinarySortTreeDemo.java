439d33e9a2a67e844476bbb378aed92036a6fbce
lizejun <846562928@qq.coom>
2019-11-08 08:47:22 +0800

    增加二叉排序树删除叶子节点的方法。

--- src/main/java/com/lzj/binarysorttree/BinarySortTreeDemo.java
+++ src/main/java/com/lzj/binarysorttree/BinarySortTreeDemo.java
@@ -8,12 +8,18 @@ package com.lzj.binarysorttree;
  */
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
         BinarySortTree binarySortTree = new BinarySortTree();
         for (int i = 0; i < arr.length; i++) {
             Node node = new Node(arr[i]);
             binarySortTree.add(node);
         }
+        System.out.println("原树");
+        binarySortTree.midOrder();
+
+        // 测试一把叶子节点的删除
+        binarySortTree.delete(2);
+        System.out.println("删除后的树");
         binarySortTree.midOrder();
     }
 }
@@ -21,26 +27,52 @@ public class BinarySortTreeDemo {
 class BinarySortTree {
     private Node root;
 
-    /**
-     * 查找要删除的节点。
-     *
-     * @param value 希望查找的值。
-     * @return 如果找到，返回Node；否则，返回null。
-     */
-    public Node search(int value) {
-        // TODO
-        return null;
+    // 删除节点
+    public void delete(int val) {
+        if (root == null) { // 如果是空树，直接返回。
+            return;
+        } else {
+            Node targetNode = search(val); // 找到这个节点。
+            if (targetNode == null) { // 如果没找到
+                return;
+            }
+            // 如果找到。
+            // 先判断找到的是否为root节点本身。即树只有一个节点的情况。
+            if (root.left == null && root.right == null) {
+                root = null;
+                return;
+            }
+            // 找到targetNode的父节点
+            Node targetParent = searchParent(val);
+            // 如果要删除节点时叶子节点
+            if (targetNode.left == null && targetNode.right == null) {
+                // 判断要删除的是父节点的左子节点还是右子节点。
+                if (targetParent.left != null && targetParent.left.value == val) {
+                    targetParent.left = null;  // 删除左边。
+                }
+                if (targetParent.right != null && targetParent.right.value == val) {
+                    targetParent.right = null; // 删除右边。
+                }
+            }
+        }
     }
 
-    /**
-     * 查找要删除节点的父节点。
-     *
-     * @param value 希望查找的值。
-     * @return 如果找到，返回父节点node；否则，返回null。
-     */
-    public Node searchParent(int value) {
-        // TODO
-        return null;
+    // 查找要删除的节点
+    public Node search(int val) {
+        if (root == null) {
+            return null;
+        } else {
+            return root.search(val);
+        }
+    }
+
+    // 查找要删除节点的父节点
+    public Node searchParent(int val) {
+        if (root == null) {
+            return null;
+        } else {
+            return root.searchParent(val);
+        }
     }
 
     // 添加节点到二叉排序树中。
@@ -70,6 +102,51 @@ class Node {
 
     public Node(int value) {
         this.value = value;
+    }
+
+    /**
+     * 查找要删除的节点。
+     *
+     * @param val 希望查找的值。
+     * @return 如果找到，返回Node；否则，返回null。
+     */
+    public Node search(int val) {
+        if (this.value == val) {
+            return this;
+        } else if (val < this.value) { // 如果比当前节点值小，就向左寻找。
+            if (this.left == null) {
+                return null;
+            }
+            return this.left.search(val);
+        } else { // 如果大于等于当前节点值，就都向右寻找。
+            if (this.right == null) {
+                return null;
+            }
+            return this.right.search(val);
+        }
+    }
+
+    /**
+     * 查找要删除节点的父节点。
+     *
+     * @param value 希望查找的值。
+     * @return 如果找到，返回父节点node；否则，返回null。
+     */
+    public Node searchParent(int value) {
+        // 如果当前节点就是要删除的节点的父节点，就返回。
+        if ((this.left != null && this.left.value == value)
+                || (this.right != null && this.right.value == value)) {
+            return this;
+        } else {
+            // 如果查找的值小于当前节点的值，并且当前节点的左子节点不为空。
+            if (value < this.value && this.left != null) {
+                return this.left.searchParent(value);
+            } else if (value >= this.value && this.right != null) { // 如果查找的值大于等于当前节点的值。
+                return this.right.searchParent(value);
+            } else {
+                return null; // 如果都没有找到，直接返回null。
+            }
+        }
     }
 
     /**
